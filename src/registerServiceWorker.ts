e80cf659f29ebbdb97296abeb16a6eb838087310
Ruslan Koldunsky <koldunsky@gmail.ru>
2019-10-20 22:22:04 +0300

    Experiments with service worker

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -13,30 +13,30 @@ window.workerState = []
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready (...args) {
-      window.workerState.push(['ready', args])
+      window.workerState.push(['ready', args, this])
       console.log(
         'App is being served from cache by a service worker.\n' +
         'For more details, visit https://goo.gl/AFskqB'
       )
     },
     registered (...args) {
-      window.workerState.push(['regisred', args])
+      window.workerState.push(['regisred', args, this])
       console.log('Service worker has been registered.')
     },
     cached (...args) {
-      window.workerState.push(['cached', args])
+      window.workerState.push(['cached', args, this])
       console.log('Content has been cached for offline use.')
     },
     updatefound (...args) {
-      window.workerState.push(['updatefound', args])
+      window.workerState.push(['updatefound', args, this])
       console.log('New content is downloading.')
     },
     updated (...args) {
-      window.workerState.push(['updated', args])
+      window.workerState.push(['updated', args, this])
       console.log('New content is available; please refresh.')
     },
     offline (...args) {
-      window.workerState.push(['offline', args])
+      window.workerState.push(['offline', args, this])
       console.log('No internet connection found. App is running in offline mode.')
     },
     error (error) {
