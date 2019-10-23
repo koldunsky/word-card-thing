d8f0f9787d7eb4f818b03bd092fc703ddba848c2
Ruslan Koldunsky <kodlunsky@gmail.com>
2019-10-23 13:46:46 +0300

    Add skip waiting event

--- src/pages/Add/Add.vue
+++ src/pages/Add/Add.vue
@@ -8,7 +8,7 @@
       <span v-if="words.length === 0">Add 3 words</span>
       <span v-if="words.length > 0">Add {{ 3 - words.length}} more</span>
     </div>
-    <label>Word<br />
+    <label>Word<br />1
       <input v-model="word" />
     </label>
     <label>Translation<br />
