1d2ec076e5cca18ad1bc4aa84c3fe8c48121881c
Ruslan Koldunsky <kodlunsky@gmail.com>
2019-10-28 20:46:45 +0300

    Replace icons

--- vue.config.js
+++ vue.config.js
@@ -4,10 +4,21 @@ module.exports = {
     name: 'KOLENKI',
     short_name: `KOLENKI`,
     description: '',
-    themeColor: '#2F4F4F',
-    msTileColor: '#2F4F4F',
+    themeColor: '#2a7e70',
+    msTileColor: '#2a7e70',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
-    orientation: 'portrait'
+    orientation: 'portrait',
+    manifestOptions: {
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url: 'https://word-card-thing.netlify.com'
+    },
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    }
   }
 }
