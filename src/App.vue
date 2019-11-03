d35a82980fd14515e9be6997f762a01976139a72
Ruslan Koldunsky <koldunsky@gmail.ru>
2019-11-04 00:11:49 +0300

    Put all three pages to one page with scroller container

--- src/App.vue
+++ src/App.vue
@@ -6,39 +6,84 @@
           'nav_visible': $store.state.words.length >= 3
          }"
     >
-      <router-link
-        class="router-link"
-        to="/"
+      <button
+        v-for="page in pages"
+        class="nav-item"
+        :class="{
+          'nav-item_active': currentPage === page
+        }"
+        :key="page"
+        @click="() => setActivePage(page)"
       >
-        Add
-      </router-link>
-      <router-link
-        class="router-link"
-        to="/drill"
-      >
-        Drill
-      </router-link>
-      <router-link
-        class="router-link"
-        to="/list"
-      >
-        List
-      </router-link>
+        {{page}}
+      </button>
     </div>
-    <router-view/>
+    <div
+      class="scroller"
+      ref="scroller"
+      @scroll="onScrollerScroll"
+    >
+      <div class="scroller__inner">
+        <Add />
+        <Drill />
+        <List />
+      </div>
+    </div>
     <UpdateChecker />
   </div>
 </template>
 
 <script lang="ts">
   import { Component, Vue } from 'vue-property-decorator'
+  import _debounce from 'lodash/debounce'
+  import Add from './pages/Add/Add.vue'
+  import Drill from './pages/Drill/index.vue'
+  import List from './pages/List/index.vue'
   import UpdateChecker from './components/updateChecker/index.vue'
 
   @Component({
     components: {
-      UpdateChecker
+      UpdateChecker,
+      Add,
+      Drill,
+      List
     }
   })
-  export default class App extends Vue {}
+  export default class App extends Vue {
+    $refs: {
+      scroller: HTMLDivElement
+    }
+
+    pages: string[] = ['Add', 'Drill', 'List']
+    currentPage: string = 'Add'
+
+    get currentPageIndex () {
+      return this.pages.indexOf(this.currentPage)
+    }
+
+    get scrollerWidth () {
+      return this.$refs.scroller.offsetWidth
+    }
+
+    setActivePage (page: string) {
+      const { scroller } = this.$refs
+      const index = this.pages.indexOf(page)
+
+      scroller.scrollTo({
+        top: 0,
+        left: this.scrollerWidth * index,
+        behavior: 'smooth'
+      })
+      this.currentPage = page
+    }
+
+    fixScrollerScroll (e) {
+      const index = Math.round(e.target.scrollLeft / this.scrollerWidth)
+
+      this.setActivePage(this.pages[index])
+    }
+
+    onScrollerScroll = _debounce(this.fixScrollerScroll, 50)
+  }
 </script>
 <style lang="scss" src="./assets/style/index.scss" ></style>
