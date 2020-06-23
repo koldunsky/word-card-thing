ff5b831a77b0b67cb3aa103d97ad68fd6488e496
Ruslan Koldunsky <koldunsky@gmail.com>
2020-06-23 22:07:44 +0200

    test caches

--- src/service-worker.js
+++ src/service-worker.js
@@ -6,6 +6,26 @@ self.addEventListener("message", event => {
   }
 });
 
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('mysite-dynamic').then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        var fetchPromise = fetch(event.request).then(function(networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        })
+        return response || fetchPromise;
+      })
+    })
+  );
+});
+
+// Will print 'my-app-install-time-v1'
+console.log(workbox.core.cacheNames.precache);
+
+// Will print 'my-app-run-time-v1'
+console.log(workbox.core.cacheNames.runtime);
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
