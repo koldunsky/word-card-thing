893a5e186e6ab517771a640d263e57b8e343b1f4
Ruslan Koldunsky <koldunsky@gmail.com>
2020-06-24 08:08:28 +0200

    test caches

--- src/service-worker.js
+++ src/service-worker.js
@@ -14,19 +14,26 @@ console.log(workbox.core.cacheNames.precache);
 // Will print 'my-app-run-time-v1'
 console.log(workbox.core.cacheNames.runtime);
 //
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.open('mysite-dynamic').then(function(cache) {
-//       return cache.match(event.request).then(function(response) {
-//         var fetchPromise = fetch(event.request).then(function(networkResponse) {
-//           cache.put(event.request, networkResponse.clone());
-//           return networkResponse;
-//         })
-//         return response || fetchPromise;
-//       })
-//     })
-//   );
-// });
+self.addEventListener('fetch', function(event) {
+  console.log(event);
+  caches.open(workbox.core.cacheNames.precache).then(function(cache) {
+    console.log('cache:', cache)
+    cache.match(event.request).then(function(response) {
+      console.log('response:', response)
+    })
+  })
+  // event.respondWith(
+  //   caches.open('mysite-dynamic').then(function(cache) {
+  //     return cache.match(event.request).then(function(response) {
+  //       var fetchPromise = fetch(event.request).then(function(networkResponse) {
+  //         cache.put(event.request, networkResponse.clone());
+  //         return networkResponse;
+  //       })
+  //       return response || fetchPromise;
+  //     })
+  //   })
+  // );
+});
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
