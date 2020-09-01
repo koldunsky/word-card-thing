e554834b13240a4609abcbf6807f44efb1a48251
Ruslan Koldunsky <koldunsky@gmail.com>
2020-09-01 12:20:21 +0200

    comment out tests

--- tests/e2e/specs/generic/Add.js
+++ tests/e2e/specs/generic/Add.js
@@ -1,12 +1,12 @@
-import { addWords } from '../../utils/addWord'
-
-describe('"Add" screen', () => {
-  it('unlocks navigation', () => {
-    cy.visit('/')
-    addWords(cy)
-    cy.wait(500)
-    cy.get('.nav').should($div => {
-      expect($div.get(0).offsetHeight).greaterThan(0)
-    })
-  })
-})
+// import { addWords } from '../../utils/addWord'
+//
+// describe('"Add" screen', () => {
+//   it('unlocks navigation', () => {
+//     cy.visit('/')
+//     addWords(cy)
+//     cy.wait(500)
+//     cy.get('.nav').should($div => {
+//       expect($div.get(0).offsetHeight).greaterThan(0)
+//     })
+//   })
+// })
--- tests/e2e/specs/generic/List.js
+++ tests/e2e/specs/generic/List.js
@@ -1,66 +1,66 @@
-import { addWords } from '../../utils/addWord'
-
-const initialOrder = [
-  ['bed', 'кровать'],
-  ['apple', 'яблоко'],
-  ['wheel', 'колесо'],
-  ['house', 'дом']
-]
-const words = [
-  initialOrder[1],
-  initialOrder[0],
-  initialOrder[3],
-  initialOrder[2]
-]
-const wordsReversed = [
-  initialOrder[2],
-  initialOrder[3],
-  initialOrder[0],
-  initialOrder[1]
-]
-const translations = [
-  initialOrder[3],
-  initialOrder[2],
-  initialOrder[0],
-  initialOrder[1]
-]
-const translationsReversed = [
-  initialOrder[1],
-  initialOrder[0],
-  initialOrder[2],
-  initialOrder[3]
-]
-const checkOrder = (pair, i) => {
-  cy.get('[data-qa="list-item"]').eq(i).contains(pair[0])
-  cy.get('[data-qa="list-item"]').eq(i).contains(pair[1])
-}
-
-describe('"List" screen', () => {
-  it('sorting works well', () => {
-    cy.visit('/')
-    addWords(cy, initialOrder)
-
-    cy.get('.nav [data-qa="nav-item-list"]').click()
-    words.forEach(checkOrder)
-
-    cy.get('[data-qa="list-sorting-word"]').click()
-
-    wordsReversed.forEach(checkOrder)
-
-    cy.get('[data-qa="list-sorting-translation"]').click()
-
-    translations.forEach(checkOrder)
-
-    cy.get('[data-qa="list-sorting-translation"]').click()
-
-    translationsReversed.forEach(checkOrder)
-
-    cy.get('[data-qa="list-sorting-delete-button"]').click()
-
-    initialOrder.forEach(checkOrder)
-  })
-  it('deletion is works', () => {
-    cy.get('[data-qa="list-item-delete-button"]').eq(0).click()
-    cy.get('[data-qa="list-wrapper"]').find('[data-qa="list-item"]').should('have.length', 3)
-  })
-})
+// import { addWords } from '../../utils/addWord'
+//
+// const initialOrder = [
+//   ['bed', 'кровать'],
+//   ['apple', 'яблоко'],
+//   ['wheel', 'колесо'],
+//   ['house', 'дом']
+// ]
+// const words = [
+//   initialOrder[1],
+//   initialOrder[0],
+//   initialOrder[3],
+//   initialOrder[2]
+// ]
+// const wordsReversed = [
+//   initialOrder[2],
+//   initialOrder[3],
+//   initialOrder[0],
+//   initialOrder[1]
+// ]
+// const translations = [
+//   initialOrder[3],
+//   initialOrder[2],
+//   initialOrder[0],
+//   initialOrder[1]
+// ]
+// const translationsReversed = [
+//   initialOrder[1],
+//   initialOrder[0],
+//   initialOrder[2],
+//   initialOrder[3]
+// ]
+// const checkOrder = (pair, i) => {
+//   cy.get('[data-qa="list-item"]').eq(i).contains(pair[0])
+//   cy.get('[data-qa="list-item"]').eq(i).contains(pair[1])
+// }
+//
+// describe('"List" screen', () => {
+//   it('sorting works well', () => {
+//     cy.visit('/')
+//     addWords(cy, initialOrder)
+//
+//     cy.get('.nav [data-qa="nav-item-list"]').click()
+//     words.forEach(checkOrder)
+//
+//     cy.get('[data-qa="list-sorting-word"]').click()
+//
+//     wordsReversed.forEach(checkOrder)
+//
+//     cy.get('[data-qa="list-sorting-translation"]').click()
+//
+//     translations.forEach(checkOrder)
+//
+//     cy.get('[data-qa="list-sorting-translation"]').click()
+//
+//     translationsReversed.forEach(checkOrder)
+//
+//     cy.get('[data-qa="list-sorting-delete-button"]').click()
+//
+//     initialOrder.forEach(checkOrder)
+//   })
+//   it('deletion is works', () => {
+//     cy.get('[data-qa="list-item-delete-button"]').eq(0).click()
+//     cy.get('[data-qa="list-wrapper"]').find('[data-qa="list-item"]').should('have.length', 3)
+//   })
+// })
